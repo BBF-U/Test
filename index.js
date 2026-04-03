d166b1f7e785e15213009374592ba632ed5bd8b3
BBF-U <vladimirx7@ukr.net>
2026-04-03 19:45:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -100,6 +100,12 @@ async function callGemini(prompt, maxTokens = 400) {
         generationConfig: {
           maxOutputTokens: maxTokens,
           temperature: 0.20
+           safetySettings: [
+    { category: "HARM_CATEGORY_HARASSMENT", threshold: "BLOCK_NONE" },
+    { category: "HARM_CATEGORY_HATE_SPEECH", threshold: "BLOCK_NONE" },
+    { category: "HARM_CATEGORY_SEXUALLY_EXPLICIT", threshold: "BLOCK_NONE" },
+    { category: "HARM_CATEGORY_DANGEROUS_CONTENT", threshold: "BLOCK_NONE" }
+  ]
         }
       })
     }
